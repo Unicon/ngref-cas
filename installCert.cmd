8b0d2cd6ee270eee96ce1916dda5bcc3551c70e8
pneff <pneff@unicon.net>
2018-03-12 14:29:55 -0700

    new

--- installCert.cmd
+++ installCert.cmd
@@ -3,7 +3,7 @@
 @Rem installed the self signed cert in the JVM keystore
 @Rem You man need to run this with admin rights
 
-"%JAVA_HOME%/bin/keytool" -import -alias "selfcert" -file self-signed.cert -keystore "%JAVA_HOME%/jre/lib/security/cacerts"
+"%JAVA_HOME%/bin/keytool" -import -alias "selfcert" -file %cd%/self-signed.cert -keystore "%JAVA_HOME%/jre/lib/security/cacerts"
 
 pause
 
