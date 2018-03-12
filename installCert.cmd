c932e4d3c09c2cdf8371eb5afa067e58a6d58a80
pneff <pneff@unicon.net>
2018-03-12 14:58:11 -0700

    new

--- installCert.cmd
+++ installCert.cmd
@@ -2,6 +2,8 @@
 
 @Rem installed the self signed cert in the JVM keystore
 @Rem You may need to run this with admin rights
+@Rem to delete the cert run
+@Rem "%JAVA_HOME%/bin/keytool" -delete -alias "selfcert" -keystore "%JAVA_HOME%/jre/lib/security/cacerts"
 
 
 "%JAVA_HOME%/bin/keytool" -import -alias "selfcert" -file c:\etc\cas\cas.cer -keystore "%JAVA_HOME%/jre/lib/security/cacerts"
