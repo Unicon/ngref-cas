f220fa2ba68cf85e9c133ad8ada184e49bdefa5d
pneff <pneff@unicon.net>
2018-03-12 14:45:55 -0700

    new

--- installCert.cmd
+++ installCert.cmd
@@ -1,12 +1,10 @@
 @echo off
 
 @Rem installed the self signed cert in the JVM keystore
-@Rem set the CERTDIR variable to the directory that containts self-signed.cert
 @Rem You may need to run this with admin rights
 
-set CERTDIR=CHANGEME
 
-"%JAVA_HOME%/bin/keytool" -import -alias "selfcert" -file %CERTDIR%/self-signed.cert -keystore "%JAVA_HOME%/jre/lib/security/cacerts"
+"%JAVA_HOME%/bin/keytool" -import -alias "selfcert" -file c:\etc\cas\cas.cer -keystore "%JAVA_HOME%/jre/lib/security/cacerts"
 
 pause
 
